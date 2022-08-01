b191311faa3f7c4bc195ff7346aebc0ccb83e701
sirwilliamwallace <shekooha696@proton.me>
2022-08-01 13:49:45 +0430

    Media and Static secure settings

--- PersonalBlog/urls.py
+++ PersonalBlog/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    # add root static files
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # add media static files
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
