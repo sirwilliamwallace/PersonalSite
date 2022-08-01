f52ca826c9b64832109da2b3b6b96cc00bdb3bd8
sirwilliamwallace <shekooha696@proton.me>
2022-08-01 13:50:16 +0430

    unique name for uploaded images

--- post/models.py
+++ post/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    name, ext = get_filename_ext(filename)
+    final_name = f"{instance.id}-{instance.title}{ext}"
+    return f"products/{final_name}"
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField()
     author = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
-    image = models.ImageField(null=True)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     isActive = models.BooleanField(default=False)
     content = models.TextField()
