95983e81cf7ea961ee4cd6cdc0c88ba0d9f57a2e
sirwilliamwallace <shekooha696@proton.me>
2022-08-01 14:44:43 +0430

    Active user is Author

--- post/models.py
+++ post/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 import os
-
 from django.urls import reverse
 from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 def get_filename_ext(filepath):
@@ -14,13 +16,13 @@ def get_filename_ext(filepath):
 def upload_image_path(instance, filename):
     name, ext = get_filename_ext(filename)
     final_name = f"{instance.id}-{instance.title}{ext}"
-    return f"products/{final_name}"
+    return f"uploaded-images/{final_name}"
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField()
-    author = models.OneToOneField() #TODO: inherit user table
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     description = models.CharField(max_length=300)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     date = models.DateTimeField(auto_now=True)
@@ -35,4 +37,3 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
-
