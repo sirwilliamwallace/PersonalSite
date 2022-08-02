8cf235779938a15956cdffd8b12ce72d1749f16e
sirwilliamwallace <shekooha696@proton.me>
2022-08-02 19:40:34 +0430

    views are implemented but i have not moved the templates yet

--- post/views.py
+++ post/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
 
-# Create your views here.
+# TODO: transfer the templates
+
 def posts_list(request):
-    pass
+    posts = models.Post.objects.all()
+    posts_count = posts.count()
+    context = {
+        "posts": posts,
+        "posts_count": posts_count,
+    }
+    return render(request, '', context)
 
 
 def post_detail(request, post_id, slug):
-    pass
+    post = get_object_or_404(models.Post, post_id, slug)
+    context = {
+        "post": post,
+    }
+    return render(request, '', context)
