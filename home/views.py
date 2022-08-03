145a89d4a55ba07411b3bb21d775984b40dc453c
sirwilliamwallace <shekooha696@proton.me>
2022-08-03 16:14:36 +0430

    Main page views

--- home/views.py
+++ home/views.py
@@ -3,4 +3,32 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_page(request):
-    return render(request, 'main/_layout.html')
+    return render(request, 'home/index.html', {})
+
+
+def contact_page(request):
+    return render(request, 'home/contact.html', {})
+
+
+def about_page(request):
+    return render(request, 'home/about.html', {})
+
+
+def services_page(request):
+    return render(request, 'home/services.html', {})
+
+
+def portfolio(request):
+    return render(request, 'home/portfolio.html', {})
+
+
+def header_component(request):
+    return render(request, 'main/components/header_component.html', {})
+
+
+def footer_component(request):
+    return render(request, 'main/components/footer_component.html', {})
+
+
+def hero_component(request):
+    return render(request, 'main/components/hero_component.html', {})
