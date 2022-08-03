1d35479af55494ade5a38d3c9f256cbbc70e96a0
sirwilliamwallace <shekooha696@proton.me>
2022-08-03 16:14:17 +0430

    Main page urls

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name='home'
 urlpatterns = [
-    path('', views.index_page, name='index_page')
+    path('', views.index_page, name='index_page'),
+    path('contact-us', views.contact_page, name='contact_page')
 ]
 
