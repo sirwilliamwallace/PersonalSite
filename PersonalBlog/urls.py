0866c74c3cd575b9746f20ba0039a3896358cac3
sirwilliamwallace <shekooha696@proton.me>
2022-08-03 12:24:07 +0430

    Home App added to urls

--- PersonalBlog/urls.py
+++ PersonalBlog/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('posts/', include('post.urls')),
+    path('', include('home.urls')),
+    path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 if settings.DEBUG:
