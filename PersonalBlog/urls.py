4ddec31531d35a54c575adfd50f74a0dde5022ae
sirwilliamwallace <shekooha696@proton.me>
2022-08-03 16:09:01 +0430

    django render partial url

--- PersonalBlog/urls.py
+++ PersonalBlog/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # apps urls
     path('posts/', include('post.urls')),
     path('', include('home.urls')),
+    # django and third-party app urls
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
