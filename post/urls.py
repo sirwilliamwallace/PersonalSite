0102bf443a3fffc6a700d5ecd942bfcd5f5eeaee
sirwilliamwallace <sirwilliamwallace>
2022-08-04 18:19:39 +0430

    fixed the slug field url

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'post'
 urlpatterns = [
-    path('', views.posts_list, name='posts'),
     path('<int:post_id>/<slug:slug>', views.post_detail, name='post-detail'),
 ]
-
