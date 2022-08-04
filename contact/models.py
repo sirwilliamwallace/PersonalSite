1b99e2cab70be7bd5221aec2db8943e6624973d3
sirwilliamwallace <sirwilliamwallace>
2022-08-04 21:46:49 +0430

    COntact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class ContactModel(models.Model):
+    title = models.CharField(max_length=300, verbose_name='Title')
+    email = models.EmailField(verbose_name='Email Address')
+    full_name = models.CharField(max_length=100, verbose_name='Full Name')
+    message = models.TextField(verbose_name='Message from Client')
+    response = models.TextField(verbose_name='Response from Admin')
+    createDate = models.DateTimeField(auto_now_add=True, verbose_name='Created date')
+    isRead = models.BooleanField(default=False, verbose_name='Is the message read by the Admin')
