a2b4af1ce126f3cbbfb5a3c70c491f3a31614d7f
sirwilliamwallace <sirwilliamwallace>
2022-08-05 01:33:34 +0430

    contact view

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
-# Create your views here.
 def contact_form(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        # print(f"{name} - {message} - {email} - {subject} - {message}")
+        return redirect(reverse('home:index_page'))
     return render(request, 'contact/contact.html', {})
