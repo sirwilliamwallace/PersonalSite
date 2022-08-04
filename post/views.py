40b7cf44306a3ef849f6479e55597af05f3a585c
sirwilliamwallace <sirwilliamwallace>
2022-08-04 18:20:36 +0430

    Views modified to show detail of posts

--- post/views.py
+++ post/views.py
@@ -5,7 +5,9 @@ from . import models
 # TODO: transfer the templates
 
 def posts_list(request):
-    posts = models.Post.objects.all().order_by('-updateDate')[:3]
+    posts = models.Post.objects.all().order_by('-createDate')
+    # for post in posts:
+    #     print(post.createDate.minute)
     context = {
         "short_description": "Learning",
         "posts": posts,
@@ -14,8 +16,12 @@ def posts_list(request):
 
 
 def post_detail(request, post_id, slug):
-    post = get_object_or_404(models.Post, post_id, slug)
+    latest_posts = models.Post.objects.all().order_by('-createDate')[:5]
+    post = get_object_or_404(models.Post, id=post_id, slug=slug)
+    categories = models.Post.objects.filter(category__post_categories__title__icontains=post.title)
     context = {
         "post": post,
+        "latest_posts": latest_posts,
+        "categories": categories,
     }
     return render(request, 'post/posts_detail.html', context)
