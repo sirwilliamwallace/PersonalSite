5a25404067399d30865dd889996b585e29ea3ceb
sirwilliamwallace <sirwilliamwallace>
2022-08-05 01:06:08 +0430

    altered response field

--- contact/models.py
+++ contact/models.py
@@ -7,7 +7,7 @@ class ContactModel(models.Model):
     email = models.EmailField(verbose_name='Email Address')
     full_name = models.CharField(max_length=100, verbose_name='Full Name')
     message = models.TextField(verbose_name='Message from Client')
-    response = models.TextField(verbose_name='Response from Admin')
+    response = models.TextField(verbose_name='Response from Admin', null=True, blank=True)
     createDate = models.DateTimeField(auto_now_add=True, verbose_name='Created date')
     isRead = models.BooleanField(default=False, verbose_name='Is the message read by the Admin')
 
