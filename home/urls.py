85d23e4319ac16213f2073350f44adb2fe40511c
sirwilliamwallace <sirwilliamwallace>
2022-08-04 18:16:21 +0430

    removed unneccessary url

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name='home'
+app_name = 'home'
 urlpatterns = [
     path('', views.index_page, name='index_page'),
-    path('contact-us', views.contact_page, name='contact_page')
 ]
-
