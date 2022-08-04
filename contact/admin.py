4f0801b4f7ec4f126c17d315954e109d572b1b68
sirwilliamwallace <sirwilliamwallace>
2022-08-04 21:49:37 +0430

    Contact Model added to the admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.ContactModel)
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ('title', 'email', 'full_name', 'createDate', 'isRead',)
+    list_filter = ('isRead', 'createDate',)
+    list_editable = ('isRead',)
+
