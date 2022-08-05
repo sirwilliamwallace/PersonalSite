3ad9f46c0b28c292f53f711f2816c2e75934fe54
sirwilliamwallace <sirwilliamwallace>
2022-08-05 15:05:27 +0430

    Made views for form

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from .forms import ContactForm
+from .forms import ContactForm, ContactUsModelForm
 
 
 def contact_form(request):
-    contactForm = ContactForm(request.POST or None)
+    # contactForm = ContactForm(request.POST or None)
+    contactForm = ContactUsModelForm(request.POST or None)
     if request.method == "POST":
         if contactForm.is_valid():
             print(contactForm.cleaned_data)
-            contactForm = ContactForm()
+            contactForm.save()
+            # contactForm = ContactForm()
+            contactForm = ContactUsModelForm()
             return render(request, 'contact/contact.html', {"contact_form": contactForm})
     return render(request, 'contact/contact.html', {"contact_form": contactForm})
