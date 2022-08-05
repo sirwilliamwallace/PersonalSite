bd5462adc076911eeb81224fdb6a8d304483c126
sirwilliamwallace <sirwilliamwallace>
2022-08-05 20:00:35 +0430

    Using FormView Now

--- contact/views.py
+++ contact/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import View
+from django.views import View
+from django.views.generic.edit import FormView
 from .forms import ContactUsModelForm
 
 
-class ContactFormView(View):
-    def get(self, request):
-        contactForm = ContactUsModelForm()
-        return render(request, 'contact/contact.html', {"contact_form": contactForm})
+class ContactFormView(FormView):
+    template_name = 'contact/contact.html'
+    form_class = ContactUsModelForm
+    success_url = '/#contact'
 
-    def post(self, request):
-        contactForm = ContactUsModelForm(request.POST)
-        if contactForm.is_valid():
-            contactForm.save()
-            contactForm = ContactUsModelForm()
-            return redirect('home:contact-form-view')
+    def form_valid(self, form):
+        form.save()
+        return super(ContactFormView, self).form_valid(form)
 
-
+# class ContactFormView(View):
+#     def get(self, request):
+#         contactForm = ContactUsModelForm()
+#         return render(request, 'contact/contact.html', {"contact_form": contactForm})
+#
+#     def post(self, request):
+#         contactForm = ContactUsModelForm(request.POST)
+#         if contactForm.is_valid():
+#             contactForm.save()
+#             contactForm = ContactUsModelForm()
+#             return redirect('home:contact-form-view')
