dc09587ec70d621108cf7fae492a6305f58dacdd
sirwilliamwallace <sirwilliamwallace>
2022-08-05 15:04:36 +0430

    Model Form

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from contact.models import ContactModel
+
 
 class ContactForm(forms.Form):
     name = forms.CharField(
@@ -52,3 +54,38 @@ class ContactForm(forms.Form):
             }
         ),
     )
+
+
+class ContactUsModelForm(forms.ModelForm):
+    class Meta:
+        model = ContactModel
+        exclude = ('createDate', 'isRead', 'response',)
+        widgets = {
+            'full_name': forms.TextInput(attrs={"type": "text", "class": "form-control", "name": "name", "id": "name",
+                                                "placeholder": "Your Name"}),
+            'email': forms.EmailInput(
+                attrs={"type": "email",
+                       "class": "form-control",
+                       "name": "email",
+                       "id": "email",
+                       "placeholder": "Your Email",
+                       },
+            ),
+            'title': forms.TextInput(
+                attrs={
+                    "type": "text",
+                    "class": "form-control",
+                    "name": "subject",
+                    "id": "subject",
+                    "placeholder": "Subject",
+                }
+            ),
+            'message': forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "name": "message",
+                    "rows": "5",
+                    "placeholder": "Message"
+                }
+            )
+        }
