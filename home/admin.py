7ed88b79e77892441bcdd9d06be8e0c01beaceb9
sirwilliamwallace <sirwilliamwallace>
2022-08-06 10:34:26 +0430

    Added new models to admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Hero, Skills, SiteSettings
+from .models import Hero, Skills, SiteSettings, Profile
 
 
 # Register your models here.
@@ -15,4 +15,9 @@ class SkillsAdmin(admin.ModelAdmin):
 
 @admin.register(SiteSettings)
 class SiteSettingsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
     pass
