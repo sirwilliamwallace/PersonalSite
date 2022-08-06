0fbe7169d193dfc5e1821e37bc628d00633aa030
sirwilliamwallace <sirwilliamwallace>
2022-08-06 13:48:41 +0430

    Rendering Files

--- contact/views.py
+++ contact/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView,CreateView
 from .forms import ContactUsModelForm
 
 
-class ContactFormView(FormView):
+class ContactFormView(CreateView):
     template_name = 'contact/contact.html'
     form_class = ContactUsModelForm
     success_url = '/#contact'
 
-    def form_valid(self, form):
-        form.save()
-        return super(ContactFormView, self).form_valid(form)
