4224dda84e8e323cb5e478f2a07114a392e343c2
sirwilliamwallace <sirwilliamwallace>
2022-08-06 07:16:59 +0430

    Dynamic Hero

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Hero(models.Model):
+    name = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Name')
+    skills = models.ManyToManyField('Skills', verbose_name='Skills')
+    bg_image = models.ImageField(verbose_name='Background Image', upload_to='back_ground_image/')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = 'Hero Section'
+        verbose_name_plural = 'Hero Section'
+
+
+class Skills(models.Model):
+    skill = models.CharField(max_length=50)
+    def __str__(self):
+        return f"{self.skill}"
