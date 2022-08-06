993c07fb9505c30a7dfbba7d457fc2360b25dd39
sirwilliamwallace <sirwilliamwallace>
2022-08-06 07:17:13 +0430

    Dynamic Hero Model added to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Hero, Skills
+
 
 # Register your models here.
+@admin.register(Hero)
+class HeroAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Skills)
+class SkillsAdmin(admin.ModelAdmin):
+    pass
