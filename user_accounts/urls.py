ff2c0c95cdb17ca9fe3b3fc8c0fbfe99d05f0f7a
sirwilliamwallace <sirwilliamwallace>
2022-08-08 20:03:23 +0430

    login_form added

--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterFormView
+from .views import RegisterFormView, LoginFormView
 
 
 app_name = 'account'
 urlpatterns = [
     path('register', RegisterFormView.as_view(), name='register'),
-    path('login', RegisterFormView.as_view(), name='login'),
+    path('login', LoginFormView.as_view(), name='login'),
 ]
