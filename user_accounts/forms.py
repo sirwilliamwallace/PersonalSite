4fe3507415206dacf9f8a2196ddd6df481414cb2
sirwilliamwallace <sirwilliamwallace>
2022-08-08 20:03:27 +0430

    login_form added

--- user_accounts/forms.py
+++ user_accounts/forms.py
@@ -1,32 +1,83 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import User
 
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        label="Username",
+        label_suffix="",
+        widget=forms.TextInput(
+            attrs={
+                "type": "text",
+                "class": "form-control-lg",
+                "id": "username",
+                "placeholder": "Your Username",
+            }
+        )
+    )
+    email = forms.CharField(
+        label="Email Address",
+        label_suffix="",
+        widget=forms.TextInput(
+            attrs={
+                "type": "email",
+                "class": "form-control-lg",
+                "id": "email",
+                "placeholder": "Your Email Address",
+            }
+        )
+    )
 
-class RegisterForm(forms.ModelForm):
     password = forms.CharField(
+        label="Password",
+        label_suffix="",
         widget=forms.PasswordInput(attrs={
             "type": "password",
-            "class": "form-control",
-            "name": "password",
+            "class": "form-control-lg",
             "id": "password",
-            "placeholder": "Your Password", })
+            "placeholder": "Your Password", }
+        )
     )
-    confirm_password = forms.PasswordInput(attrs={
-        "type": "password",
-        "class": "form-control",
-        "name": "confirm_password",
-        "id": "confirm_password",
-        "placeholder": "Confirm Your Password", })
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password',)
+    confirm_password = forms.CharField(
+        label="Confirm Password",
+        label_suffix="",
+        widget=forms.PasswordInput(attrs={
+            "type": "password",
+            "class": "form-control-lg",
+            "id": "confirm_password",
+            "placeholder": "Confirm Your Password",
+        }))
 
-    def clean_password(self):
+    def clean_confirm_password(self):
         password = self.cleaned_data.get('password')
         confirm_password = self.cleaned_data.get('confirm_password')
         if password == confirm_password:
             return confirm_password
         raise ValidationError('Passwords dont match')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label="Username",
+        label_suffix="",
+        widget=forms.TextInput(
+            attrs={
+                "type": "text",
+                "class": "form-control-lg",
+                "id": "username",
+                "placeholder": "Your Username",
+            }
+        )
+    )
+
+    password = forms.CharField(
+        label="Password",
+        label_suffix="",
+        widget=forms.PasswordInput(attrs={
+            "type": "password",
+            "class": "form-control-lg",
+            "id": "password",
+            "placeholder": "Your Password", }
+        )
+    )
