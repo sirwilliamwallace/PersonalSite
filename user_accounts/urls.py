ef3fd3ac146fd18335321913ce4bdd4b3939289d
sirwilliamwallace <sirwilliamwallace>
2022-08-09 11:28:41 +0430

    Logtout url

--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterFormView, LoginFormView
+from .views import RegisterFormView, LoginFormView, LogoutView,ActivateView
 
 
 app_name = 'account'
 urlpatterns = [
     path('register', RegisterFormView.as_view(), name='register'),
     path('login', LoginFormView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('activate-account/<activation_code>', ActivateView.as_view(), name='activation_code'),
 ]
