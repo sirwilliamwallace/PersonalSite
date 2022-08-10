38ecb84ee06f46ec02959b442c82128535a18cdd
sirwilliamwallace <sirwilliamwallace>
2022-08-10 15:39:24 +0430

    Initial email sending

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -150,3 +150,10 @@ CKEDITOR_CONFIGS = {
         'toolbar': 'Basic'
     }
 }
+EMAIL_BACKEND = 'django.core.mail.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+
