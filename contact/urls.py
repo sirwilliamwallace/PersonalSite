db9584fb7cef320abb00d0e315b3f2f7baf12d6c
sirwilliamwallace <sirwilliamwallace>
2022-08-13 15:58:38 +0430

    new url pattern for contact

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,5 @@ from .views import ContactFormView
 
 app_name = 'contact-me'
 urlpatterns = [
-    path('', ContactFormView.as_view(), name="contact-form"),
+    path('#contact', ContactFormView.as_view(), name="contact-form"),
 ]
