2f78ee00cf5b9f70c083df9c932fa44f71925f9f
sirwilliamwallace <sirwilliamwallace>
2022-08-14 14:55:11 +0430

    Automatic author

--- post/admin.py
+++ post/admin.py
@@ -3,6 +3,9 @@ from . import models
 
 
 # Register your models here.
+from .models import Post
+
+
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {
@@ -13,10 +16,16 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description', 'content',)
     list_editable = ('isActive',)
 
+    def save_model(self, request, obj: Post, form, change):
+        if not change:
+            obj.author = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
+
+
 
 @admin.register(models.PostCategory)
 class PostCategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'url_title', 'isDelete', 'isActive','parent_model')
+    list_display = ('title', 'url_title', 'isDelete', 'isActive', 'parent_model')
     search_fields = ('title', 'url_title',)
 
 
