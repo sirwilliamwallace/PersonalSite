72b6e21d702dd8e0dfe4a00da171e5f9126aeeed
sirwilliamwallace <sirwilliamwallace>
2022-08-14 11:56:40 +0430

    Reformat post models

--- post/models.py
+++ post/models.py
@@ -9,17 +9,41 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=300, unique=True, verbose_name='Title')
-    category = models.ManyToManyField('PostCategory', related_name='post_categories', verbose_name='Categories')
-    slug = models.SlugField(max_length=300, default="", null=False, db_index=True, unique=True,
-                            verbose_name='Slug (Auto-complete)')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', verbose_name='Author')
-    description = models.CharField(max_length=300, verbose_name='Short Description')
+    title = models.CharField(max_length=300,
+                             unique=True,
+                             verbose_name='Title'
+                             )
+    category = models.ManyToManyField('PostCategory',
+                                      related_name='post_categories',
+                                      verbose_name='Categories'
+                                      )
+    slug = models.SlugField(max_length=300,
+                            default=""
+                            ,null=False,
+                            db_index=True,
+                            unique=True,
+                            verbose_name='Slug (Auto-complete)'
+                            )
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts',
+                               verbose_name='Author'
+                               )
+    description = models.CharField(max_length=300,
+                                   verbose_name='Short Description'
+                                   )
     content = RichTextUploadingField(verbose_name='Content')
-    createDate = models.DateTimeField(auto_now_add=True, verbose_name='Created Date')
-    updateDate = models.DateTimeField(auto_now=True, verbose_name='Updated Date')
-    image = models.ImageField(upload_to=f'post_images/', null=True,blank=True, verbose_name='Image')
-    isActive = models.BooleanField(default=False, verbose_name='Active / Inactive')
+    createDate = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='Created Date')
+    updateDate = models.DateTimeField(auto_now=True,
+                                      verbose_name='Updated Date')
+    image = models.ImageField(upload_to=f'post_images/',
+                              null=True,
+                              blank=True,
+                              verbose_name='Image'
+                              )
+    isActive = models.BooleanField(default=False,
+                                   verbose_name='Active / Inactive')
     isDelete = models.BooleanField(verbose_name='Deleted')
 
     def __str__(self):
@@ -37,8 +61,13 @@ class Post(models.Model):
 
 
 class PostTag(models.Model):
-    caption = models.CharField(max_length=300, db_index=True, verbose_name='Caption')
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='post_tags', verbose_name='Post')
+    caption = models.CharField(max_length=300,
+                               db_index=True,
+                               verbose_name='Caption')
+    post = models.ForeignKey('Post',
+                             on_delete=models.CASCADE,
+                             related_name='post_tags',
+                             verbose_name='Post')
 
     class Meta:
         verbose_name = "Post's tag"
@@ -55,9 +84,14 @@ class PostCategory(models.Model):
                                      null=True,
                                      blank=True
                                      )
-    title = models.CharField(max_length=300, db_index=True, verbose_name='Title')
-    url_title = models.CharField(max_length=300, db_index=True, verbose_name='Url Title')
-    isActive = models.BooleanField(default=False, verbose_name='Active / Inactive')
+    title = models.CharField(max_length=300,
+                             db_index=True,
+                             verbose_name='Title')
+    url_title = models.CharField(max_length=300,
+                                 db_index=True,
+                                 verbose_name='Url Title')
+    isActive = models.BooleanField(default=False,
+                                   verbose_name='Active / Inactive')
     isDelete = models.BooleanField(verbose_name='Deleted')
 
     def __str__(self):
