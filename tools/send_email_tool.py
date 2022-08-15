57fef3ecdcf04153d3a98a8f971527a4f3561dab
sirwilliamwallace <sirwilliamwallace>
2022-08-15 13:32:39 +0430

    Reformat file

--- tools/send_email_tool.py
+++ tools/send_email_tool.py
@@ -5,12 +5,20 @@ from django.conf import settings
 
 
 def send_email(subject, to, context, template_name):
+    """
+    Method for sending email
+    """
     try:
         html_message = render_to_string(template_name, context)
-        plain_message = strip_tags(html_message)
-        from_email = settings.EMAIL_HOST_USER
-        send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+        plain_message, from_email = strip_tags(html_message), settings.EMAIL_HOST_USER
+        send_mail(subject,
+                  plain_message,
+                  from_email,
+                  [to],
+                  html_message=html_message)
     except Exception as e:
-        print(e)
-        mail_admins("ERROR occurred in sending email", message=e, fail_silently=True)
+        # print(e)
+        mail_admins("ERROR occurred in sending email",
+                    message=e,
+                    fail_silently=True)
         pass
