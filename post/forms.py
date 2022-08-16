05fe58917a62cd8c04258ffd64ec21f8f66dbde7
sirwilliamwallace <shekooha696@proton.me>
2022-08-16 12:20:07 +0430

    Comment Form

--- post/forms.py
+++ post/forms.py
@@ -1,8 +1,21 @@
 from django import forms
+from django.core import validators
 from .models import PostComment
 
 
 class CommentForm(forms.ModelForm):
+    comment_text = forms.CharField(widget=forms.Textarea(attrs={
+        "name": "message",
+        "id": "textMessage",
+        "class": "form-control input-mf",
+        "placeholder": "Comment *",
+        "cols": "45",
+        "rows": "8"
+    }),
+        required=True,
+        validators=[validators.ProhibitNullCharactersValidator, ]
+    )
+
     class Meta:
         model = PostComment
         fields = ('comment_text',)
