6bf73a8b15eb5edb5f5f6b036033763485dd63e2
sirwilliamwallace <shekooha696@proton.me>
2022-08-18 20:41:05 +0430

    Url for search

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostDetailView
+from .views import PostDetailView, SearchPostsView
 
 app_name = 'post'
 urlpatterns = [
     path('<int:post_id>/<slug:slug>', PostDetailView.as_view(), name='post-detail'),
+    path('', SearchPostsView.as_view(), name='search'),
 ]
