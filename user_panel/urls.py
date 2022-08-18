b76463fe0348b2ed02f657a3fbcd7c4dc4e265f6
sirwilliamwallace <shekooha696@proton.me>
2022-08-18 10:50:41 +0430

    Url for password change

--- user_panel/urls.py
+++ user_panel/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DashboardView, EditProfileView
+from .views import DashboardView, EditProfileView, ChangePasswordView
 
 app_name = "user_panel"
 urlpatterns = [
     path('dashbord', DashboardView.as_view(), name="dashboard"),
+    path('change-password', ChangePasswordView.as_view(), name="change-password"),
     path('edit-info', EditProfileView.as_view(), name="edit-info"),
 ]
